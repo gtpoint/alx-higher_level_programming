327405ee4c2f66fbe4eac1ab71ac5cfc353cd03e
gtpoint <elaouzariofficiel12@gmail.com>
2023-06-13 00:21:34 +0000

    created file 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#2-replace_in_list.py
+
+def replace_in_list(my_list, idx, element):
+    """replaces an element of a list at a specific position (like in C)."""
+
+    if idx < 0 or idx >= len(my_list):
+       return (my_list)
+
+    my_list[idx] = element
+    return (my_list)
