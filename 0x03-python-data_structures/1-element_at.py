cd3d354f8a8282f7362eba7fa8ccf7c6225ad87f
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-06-13 02:02:48 +0200

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,7 +6,7 @@ def element_at(my_list, idx):
     if idx < 0:
        return None
     
-    elif idx > len(my_list):
+    if idx > len(my_list):
        return None
-    else:
-       return my_list[idx]
+    
+    return my_list[idx]
