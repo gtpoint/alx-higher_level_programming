3c0b2c0bf82fa1a3b328ecf14ce18450f1742933
gtpoint <elaouzariofficiel12@gmail.com>
2023-09-12 08:21:32 -0700

    created new files

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length < 3) {
   console.log(0);
 } else {
-  let nb_sec_max = process.argv.sort()[process.argv - 2];
+  let nb_sec_max = process.argv.slice(2).sort()[process.argv - 4];
   console.log(nb_sec_max);
 }
