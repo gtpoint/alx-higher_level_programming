276b0054747e4688db854c4e7be326550bd7568c
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-12 23:41:08 +0200

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length < 3) {
   console.log(0);
 } else {
-  let nb_sec_max = process.argv.slice(2).sort()[process.argv - 4];
-  console.log(nb_sec_max);
+  let nb_sec_max = process.argv.slice(2).sort((a, b) => b - a);
+  console.log(nb_sec_max[nb_sec_max.length - 2]);
 }
