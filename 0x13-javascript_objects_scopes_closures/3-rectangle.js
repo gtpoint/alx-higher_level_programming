df55b6ec3f0cb4227aab397cd2d1911bcef74f79
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-13 16:38:01 +0200

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,8 +4,6 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
   print () {
-    for (let i = 0; i < this.height; i++)
-      for (let j = 0; j < this.width; j++)
-        console.log("X");
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
