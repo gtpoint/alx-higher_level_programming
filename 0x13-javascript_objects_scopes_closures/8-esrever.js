3d45e5e8c779ddec9eb6c48ca4f201edc4bb9dea
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-13 19:16:12 +0200

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,5 @@
-#!/usr/bin/node
 exports.esrever = function (list) {
-  for (let i = 1; i < list.length / 2; i++)
-    [list[i]] = [list[list.length - i]];
-  return list;
-}
+  return list.map((el, index, arr) =>{
+    return arr[list.length - index]
+  })
+};
