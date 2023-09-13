abac8a80783fde66bce3c89fc2b4ac38d0108343
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-13 16:34:57 +0200

    Create 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++)
+      for (let j = 0; j < this.width; j++)
+        console.log("X");
+  }
+};
