fc83542b03a38bb45aed1eb782df35be33c7c860
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-14 16:54:37 +0200

    Create 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Lists all states from the database hbtn_0e_0_usa.
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+  db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+  crs = db.cursor()
+  crs.execute("SELECT * FROM `states` WHERE `states`.name LIKE `N%`")
+  for stat in crs.fetchall():
+    print(stat)
