e656dd1f64ca677ed03f0be77debd221214311c1
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-15 18:54:41 +0200

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,9 +7,8 @@ import MySQLdb
 if __name__ == "__main__":
   db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
   crs = db.cursor()
-  crs.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
-              FROM `cities` JOIN `states` \
-              ON `cities`.`state_id` = `states`.`id` \
-              ORDER BY `states`.`id` ASC")
+  c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` ORDER BY `c`.`id`")
   for stat in crs.fetchall():
     print(stat)
