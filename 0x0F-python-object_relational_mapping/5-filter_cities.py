ceeacfa2ab484fc457e1e2c3b0e8ddfd36e27880
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-16 00:06:19 +0200

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,12 +7,10 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-  db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-  crs = db.cursor()
-  crs.execute("SELECT * FROM `cities` as `c` \
-                JOIN `states` as `s` \
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
-  for citie in crs.fetchall():
-    if citie[3] == sys.argv[4]:
-      print(citie[3])
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
