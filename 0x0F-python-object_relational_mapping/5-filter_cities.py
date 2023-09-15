167344015487a04c042781c5cae57cbc0c5bd301
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-15 19:12:56 +0200

    Create 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Displays all cities of a given state from the
+# states table of the database hbtn_0e_4_usa.
+# Safe from SQL injections.
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+  db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+  crs = db.cursor()
+  crs.execute("SELECT * FROM `cities` as `c` \
+                JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+  for citie in crs.fetchall():
+    if citie[3] == sys.argv[4]:
+      print(citie[3])
