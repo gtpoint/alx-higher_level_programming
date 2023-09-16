a360eab7b51c249b70fbbe42697a1fd79782b31b
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-16 18:41:07 +0200

    Update 6-model_state.py

--- 0x0F-python-object_relational_mapping/tests/6-model_state.py
+++ 0x0F-python-object_relational_mapping/tests/6-model_state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Start link class to table in database
 """
-import sys
-sys.path.append('/path/to/0x0F-python-object_relational_mapping')
 
+import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 
