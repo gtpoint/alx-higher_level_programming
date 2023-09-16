02568054e2421b3bccabe5e42f66364c53471dee
gtpoint <elaouzariofficiel12@gmail.com>
2023-09-16 08:51:13 -0700

    created new file

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    crs = db.cursor()
+    crs.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in crs.fetchall()]
